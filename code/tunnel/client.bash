9715453c14812ced15dda5529ca4f5d8064d33a5
Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>
2023-10-19 15:17:28 +0400

    Work in progress

--- code/tunnel/client.bash
+++ code/tunnel/client.bash
@@ -5,6 +5,7 @@ if test -f "$SOCKET"; then
   rm "$SOCKET"
 fi
 
-waypipe  -s "$SOCKET" client
+echo "Starting waypipe in background"
+waypipe  -s "$SOCKET" client &
 sudo rmmod kvm_ivshmem ; sudo insmod ../module/kvm_ivshmem.ko; sudo chmod a+rwx /dev/ivshmem
 ./server
