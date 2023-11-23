8c5cd4b907da15649c788d465d05ef3d06655b5c
Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>
2023-11-23 16:48:33 +0400

    Prepared merge to main

--- memsocket/memsocket.c
+++ memsocket/memsocket.c
@@ -1,4 +1,5 @@
-/* Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
+/* 
+   Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
    SPDX-License-Identifier: Apache-2.0
 */
 #include <arpa/inet.h>
